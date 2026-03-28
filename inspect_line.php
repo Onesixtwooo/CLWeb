37e01afc7b7122733c1e347aa835a8d30232ecea
Onesixtwooo <catabona.odie132004@gmail.com>
2026-03-29 01:37:06 +0800

    Remove additional root helper files

--- inspect_line.php
+++
@@ -1,7 +0,0 @@
-<?php
-$file = 'd:/htdocs/CLSU/resources/views/includes/college-header.blade.php';
-$lines = file($file);
-$line85 = $lines[84]; // 0-indexed
-echo "Line 85: " . bin2hex($line85) . "\n";
-echo "Content: " . $line85 . "\n";
-?>
--- read_log.php
+++
@@ -1,9 +0,0 @@
-<?php
-$log = file_get_contents('d:/htdocs/CLSU/storage/logs/laravel.log');
-// Find the last occurrence of "About images request data"
-$pos = strrpos($log, "About images request data");
-if ($pos !== false) {
-    echo substr($log, $pos, 4000);
-} else {
-    echo "NOT FOUND";
-}
